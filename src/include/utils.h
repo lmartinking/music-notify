af377bd71095c0167c596861943b86762c787206
lmartinking <lmartinking@gmail.com>
2006-09-08 12:00:01 +0000

    * made NLS optional * added DEBUG() macro
    git-svn-id: http://lmk-hacks.googlecode.com/svn/trunk/music-notify@12 0356bdc1-9c19-0410-90ee-d130ae4ae5ca

--- src/include/utils.h
+++ src/include/utils.h
@@ -6,14 +6,21 @@
 #include <stdio.h>	/* for fprintf, etc */
 #include <stdlib.h>	/* for exit() */
 
+#ifdef NLS
 //#include <locale.h>
 //#include <libintl.h>	/* for i18n! */
 //#define _(string)	gettext(string)
+#endif
+
 #define _(string)	string
 
+extern int debug;
+
+#define DEBUG(args...) 	{ if (debug) { fprintf(stderr, "[%s:%d] %s(): ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, args); } }
 #define ERROR(args...)	fprintf(stderr, args)
 #define WARN(args...)	fprintf(stderr, args)
 #define INFO(args...)	printf(args)
 #define DIE()		exit(EXIT_FAILURE)
+#define EXIT()		exit(EXIT_SUCCESS)
 
 #endif /* __UTILS_H_ */
