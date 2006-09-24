8cfeaea1f44503a606026df2c4678b4db7695b85
lmartinking <lmartinking@gmail.com>
2006-09-24 02:53:23 +0000

    Different behavior if a module (the debug symbol isn't shared as yet)
    git-svn-id: http://lmk-hacks.googlecode.com/svn/trunk/music-notify@13 0356bdc1-9c19-0410-90ee-d130ae4ae5ca

--- src/include/utils.h
+++ src/include/utils.h
@@ -16,7 +16,12 @@
 
 extern int debug;
 
+#ifdef __MODULE__
+#define DEBUG(args...)	if (getenv("DEBUG")) { fprintf(stderr, "[%s:%d] %s(): ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, args); }
+#else
 #define DEBUG(args...) 	{ if (debug) { fprintf(stderr, "[%s:%d] %s(): ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, args); } }
+#endif
+
 #define ERROR(args...)	fprintf(stderr, args)
 #define WARN(args...)	fprintf(stderr, args)
 #define INFO(args...)	printf(args)
